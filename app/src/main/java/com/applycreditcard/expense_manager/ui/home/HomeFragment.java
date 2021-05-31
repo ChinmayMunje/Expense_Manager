c4594d0bfa9487e322c67cfd3d92b15d769f1149
Chinmay Munje <chinmaymunje96@gmail.com>
2021-05-31 14:57:33 +0530

    Resolved Issue.

--- app/src/main/java/com/applycreditcard/expense_manager/ui/home/HomeFragment.java
+++ app/src/main/java/com/applycreditcard/expense_manager/ui/home/HomeFragment.java
@@ -2,7 +2,6 @@ package com.applycreditcard.expense_manager.ui.home;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,49 +15,45 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.applycreditcard.expense_manager.CustomeAdapter;
 import com.applycreditcard.expense_manager.HomeScreenListAdapter;
 import com.applycreditcard.expense_manager.HomeScreenListData;
-import com.applycreditcard.expense_manager.MainActivity;
 import com.applycreditcard.expense_manager.R;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import java.util.Calendar;
 
 public class HomeFragment extends Fragment {
-//    final Context context = this;
+    //    final Context context = this;
     // Button button;
     ListView simpleList;
-    String categoryList[] = {"Clothing", "Food", "Fuel", "Health", "Recharge", "Electricity Bill","Salary"};
+    String categoryList[] = {"Clothing", "Food", "Fuel", "Health", "Recharge", "Electricity Bill", "Salary"};
     int flags[] = {R.drawable.ic_clothing, R.drawable.ic_food, R.drawable.ic_gas_pump, R.drawable.ic_health, R.drawable.ic_mobile_recharge, R.drawable.ic_lightning, R.drawable.ic_wallet};
     private LinearLayout bottomSheetLL;
 
     HomeScreenListData[] homeScreenListData = new HomeScreenListData[]{
-     new HomeScreenListData(R.drawable.ic_clothing,"Clothing","500"),
-            new HomeScreenListData(R.drawable.ic_clothing,"Clothing","500"),
-            new HomeScreenListData(R.drawable.ic_clothing,"Clothing","500"),
-            new HomeScreenListData(R.drawable.ic_clothing,"Clothing","500"),
-            new HomeScreenListData(R.drawable.ic_clothing,"Clothing","500"),
-            new HomeScreenListData(R.drawable.ic_clothing,"Clothing","500"),
-            new HomeScreenListData(R.drawable.ic_clothing,"Clothing","500"),
-            new HomeScreenListData(R.drawable.ic_clothing,"Clothing","500"),
-            new HomeScreenListData(R.drawable.ic_clothing,"Clothing","500"),
-            new HomeScreenListData(R.drawable.ic_clothing,"Clothing","500"),
+            new HomeScreenListData(R.drawable.ic_clothing, "Clothing", "500"),
+            new HomeScreenListData(R.drawable.ic_clothing, "Clothing", "500"),
+            new HomeScreenListData(R.drawable.ic_clothing, "Clothing", "500"),
+            new HomeScreenListData(R.drawable.ic_clothing, "Clothing", "500"),
+            new HomeScreenListData(R.drawable.ic_clothing, "Clothing", "500"),
+            new HomeScreenListData(R.drawable.ic_clothing, "Clothing", "500"),
+            new HomeScreenListData(R.drawable.ic_clothing, "Clothing", "500"),
+            new HomeScreenListData(R.drawable.ic_clothing, "Clothing", "500"),
+            new HomeScreenListData(R.drawable.ic_clothing, "Clothing", "500"),
+            new HomeScreenListData(R.drawable.ic_clothing, "Clothing", "500"),
     };
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        Button button  = root.findViewById(R.id.addTransaction);
-                button.setOnClickListener(new View.OnClickListener() {
+        Button button = root.findViewById(R.id.addTransaction);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 displayBottomSheet();
